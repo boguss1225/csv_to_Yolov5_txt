8f49e2255d347297d76dedc9ca0c5514ab76095f
boguss1225 <jcjc7890@gmail.com>
2023-05-23 16:39:09 +1000

    label numbering function is added

--- csv_to_Yolo_txt.py
+++ csv_to_Yolo_txt.py
@@ -5,11 +5,53 @@ from pathlib import Path
 from tqdm import tqdm
 
 # Path to be configured
-source = "/Users/bogus/Desktop/dst/data_test.csv"
-dst = "/Users/bogus/Desktop/dst/"
+source = "../images-original.csv"
+dst = "../images-original/"
 
 # Open CSV file
 input_file = csv.DictReader(open(source))
+
+def label_process(label):
+    if label == "money" :
+        return 0
+    elif label == "animal_alive" :
+        return 1
+    elif label == "animal_dead" :
+        return 2
+    elif label == "animal_skin" :
+        return 3
+    elif label == "low_animal_alive" :
+        return 4
+    elif label == "low_animal_dead" :
+        return 5
+    elif label == "low_animal_skin" :
+        return 6
+    elif label == "muzina_alive" :
+        return 7
+    elif label == "muzina_dead" :
+        return 8
+    elif label == "muzina_skin" :
+        return 9
+    elif label == "orc_alive" :
+        return 10
+    elif label == "orc_dead" :
+        return 11
+    elif label == "orc_skin" :
+        return 12
+    elif label == "scol_alive" :
+        return 13
+    elif label == "scol_dead" :
+        return 14
+    elif label == "scol_skin" :
+        return 15
+    elif label == "mans_alive" :
+        return 16
+    elif label == "mans_dead" :
+        return 17
+    elif label == "mans_skin" :
+        return 18
+    else :
+        print("label error", label)
 
 for line in tqdm(list(input_file)):
     # define file name
@@ -37,10 +79,14 @@ for line in tqdm(list(input_file)):
     w = str(round(w,6))
     h = str(round(h,6))
 
+
+    # process label
+    labelname = str(label_process(line['class']))
+
     # write to file
     if(os.path.isfile(file_name)):
         with open(file_name,'a+') as f:
-            f.write(line['class']+" "+x+" "+y+" "+w+" "+h+'\n')
+            f.write(labelname +" "+x+" "+y+" "+w+" "+h+'\n')
     else:
         with open(file_name,'w') as f:
-            f.write(line['class']+" "+x+" "+y+" "+w+" "+h+'\n')    
+            f.write(labelname +" "+x+" "+y+" "+w+" "+h+'\n')    
